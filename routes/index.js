a3db4ea64bcd76dbfa4998513343e579354c396e
SamruddhiV12 <70433900+SamruddhiV12@users.noreply.github.com>
2025-11-04 23:22:59 -0600

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,6 @@
  */
 
 module.exports = function (app, router) {
-  // Mount /api/users and /api/tasks from home.js
-  const homeRoutes = require('./home.js')(router);
-
-  app.use('/api/tasks', homeRoutes);
-  app.use('/api/users', homeRoutes);
+    // Attach home.js (which contains /api/users and /api/tasks)
+    app.use('/api', require('./home.js')(router));
 };
